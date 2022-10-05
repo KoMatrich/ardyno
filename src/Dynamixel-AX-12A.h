cd408ab7b9e2aeee8d1a14dc008249226f14cea9
KoMatrich <kocichmartin@gmail.com>
2022-10-05 13:33:38 +0200

    Merged multiple addresses under single name

---
+++ src/Dynamixel-AX-12A.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <Dynamixel.h>
+#include <DynamixelMotor.h>
+
+class AX_12A_Address
+{
+    using enum DynCommonAddress;
+    using enum DynMotorAdditionalAddress;
+    using enum DynMotorAddress;
+};
